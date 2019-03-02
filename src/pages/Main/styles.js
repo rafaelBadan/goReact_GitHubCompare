6df05942bc8f4f47f6f5a0aac65a0a19a856e8a8
rafaelBadan <rafael.badan@gmail.com>
2019-03-02 12:29:37 -0300

    creates CompareList component

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,23 +1,23 @@
-import React from 'react';
 import styled from 'styled-components';
 
-import logo from '../assets/logo.png';
-
-const Container = styled.div`
+export const Container = styled.div`
   display: flex;
   flex-direction: column;
+
   align-items: center;
   padding-top: 60px;
 `;
 
-const Form = styled.form`
+export const Form = styled.form`
+  display: flex;
+
   margin-top: 20px;
   width: 100%;
   max-width: 400px;
-  display: flex;
 
   input {
     flex: 1;
+
     height: 55px;
     padding: 0 20px;
     background: #fff;
@@ -37,18 +37,9 @@ const Form = styled.form`
     font-size: 20px;
     font-weight: bold;
     border-radius: 3px;
+
+    &:hover {
+      background: #52d89f;
+    }
   }
 `;
-
-const Main = () => (
-  <Container>
-    <img src={logo} alt="GitHub Compare" />
-
-    <Form>
-      <input type="text" placeholder="usuário/repositório" />
-      <button type="submit">OK</button>
-    </Form>
-  </Container>
-);
-
-export default Main;
