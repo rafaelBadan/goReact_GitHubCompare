affcab4279915da73b5b8a6f2745cc726435b2e9
rafaelBadan <rafael.badan@gmail.com>
2019-03-04 11:41:18 -0300

    Updates

---
+++ precache-manifest.d0abf389fbae1b2d46e9e2c12e354b83.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/goReact_GitHubCompare/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/goReact_GitHubCompare/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/goReact_GitHubCompare/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/goReact_GitHubCompare/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/goReact_GitHubCompare/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "f78b2d83aa75956bb7ba",
+    "url": "/goReact_GitHubCompare/static/js/runtime~main.f78b2d83.js"
+  },
+  {
+    "revision": "fe6f7dc4a9970667aee6",
+    "url": "/goReact_GitHubCompare/static/js/main.fe6f7dc4.chunk.js"
+  },
+  {
+    "revision": "80d87c1da1c10ad5d48d",
+    "url": "/goReact_GitHubCompare/static/js/2.80d87c1d.chunk.js"
+  },
+  {
+    "revision": "80d87c1da1c10ad5d48d",
+    "url": "/goReact_GitHubCompare/static/css/2.3666fce2.chunk.css"
+  },
+  {
+    "revision": "dba147c3247403665bea93d4ceceaf4f",
+    "url": "/goReact_GitHubCompare/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6ac7c7a4b0021d9e93ae030c197c74da.js"
+  "/goReact_GitHubCompare/precache-manifest.d0abf389fbae1b2d46e9e2c12e354b83.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/goReact_GitHubCompare/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
